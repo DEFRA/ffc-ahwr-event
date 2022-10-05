987bd8b9bb0fcf91be36e82f284379f40f9dfdd2
Steve Dickinson <32902665+steve-dickinson@users.noreply.github.com>
2022-10-05 22:13:14 +0100

    Add monitoring events
    * Update to schema and storage
    
    * Add cph and cph to partition and row key
    
    * Add monitoring events

--- ffc-ahwr-event/index.js
+++ ffc-ahwr-event/index.js
@@ -1,11 +1,16 @@
 const { saveEvent } = require('./event')
+const { saveMonitoring } = require('./monitoring')
 const { validateEvent } = require('./event-schema')
 
 module.exports = async function (context, message) {
   const event = message
   context.log.info(`Received event: ${JSON.stringify(event)}`)
 
-  if (validateEvent(event)) {
-    await saveEvent(context, event)
+  if (event.name === 'send-session-event') {
+    if (validateEvent(event)) {
+      await saveEvent(context, event)
+    }
+  } else {
+    await saveMonitoring(context, event)
   }
 }
