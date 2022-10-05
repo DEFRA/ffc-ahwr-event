4d8d36c7e79b5f9c041961abfc8101598d51dab3
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-10-05 22:06:58 +0100

    Add monitoring events

---
+++ ffc-ahwr-event/monitoring.js
@@ -0,0 +1,29 @@
+const saveMonitoring = async (context, event) => {
+  const raisedEvent = event.properties
+  const eventType = raisedEvent.action.type
+  const eventRaised = new Date(raisedEvent.action.raisedOn)
+  const eventRaisedBy = raisedEvent.action.raisedBy ?? 'unknown'
+  const timespan = new Date(raisedEvent.action.raisedOn).getTime()
+
+  const partitionKey = 'monitoring'
+  const rowKey = `monitoring_${timespan}`
+  const sessionId = raisedEvent.id.toString()
+
+  const eventMonitoringLog = {
+    PartitionKey: partitionKey,
+    RowKey: rowKey,
+    SessionId: sessionId,
+    EventType: eventType,
+    EventRaised: eventRaised,
+    EventBy: eventRaisedBy,
+    Payload: JSON.stringify(raisedEvent.action),
+    Status: event.properties.status
+  }
+
+  context.bindings.tableMonitoringBinding = []
+  context.bindings.tableMonitoringBinding.push(eventMonitoringLog)
+
+  context.log.info(`Monitoring Event saved successfully: partitionKey: ${partitionKey}, rowKey: ${rowKey}`)
+}
+
+module.exports = { saveMonitoring }
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,9 @@
 jest.mock('../ffc-ahwr-event/event')
 const mockEvent = require('../ffc-ahwr-event/event')
 
+jest.mock('../ffc-ahwr-event/monitoring')
+const mockMonitoringEvent = require('../ffc-ahwr-event/monitoring')
+
 const processEvent = require('../ffc-ahwr-event/index')
 const mockContext = require('./mock-context')
 
@@ -9,7 +12,7 @@ let message
 describe('index function', () => {
   beforeEach(async () => {
     message = {
-      name: 'test',
+      name: 'send-session-event',
       properties: {
         id: '123456789',
         sbi: '123456789',
@@ -31,18 +34,24 @@ describe('index function', () => {
     jest.resetAllMocks()
   })
 
-  test('receives message from service bus and successfully calls save', async () => {
+  test('receives message from service bus and successfully calls save event', async () => {
     await processEvent(mockContext, message)
     expect(mockEvent.saveEvent).toHaveBeenCalledTimes(1)
   })
 
-  test('receives message from service bus with invalid id and does not calls save', async () => {
+  test('receives message from service bus and successfully calls save monitoring event', async () => {
+    message.name = 'send-monitoring-event'
+    await processEvent(mockContext, message)
+    expect(mockMonitoringEvent.saveMonitoring).toHaveBeenCalledTimes(1)
+  })
+
+  test('receives message from service bus with invalid id and does not calls save event', async () => {
     message.properties.id = 123456789
     await processEvent(mockContext, message)
     expect(mockEvent.saveEvent).toHaveBeenCalledTimes(0)
   })
 
-  test('receives message from service bus with no action property and does not calls save', async () => {
+  test('receives message from service bus with no action property and does not calls save event', async () => {
     delete message.properties.action
     await processEvent(mockContext, message)
     expect(mockEvent.saveEvent).toHaveBeenCalledTimes(0)
