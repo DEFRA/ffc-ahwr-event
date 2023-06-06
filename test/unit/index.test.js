d844f4a3037c1f759d1d4c634404e3007b10ea19
Jason Hamilton <115159962+json-hamilton@users.noreply.github.com>
2023-06-06 10:43:09 +0100

    FFC-187745: Exception Event (#15)
    * Added new exception event handling
    
    * Version bump
    
    * Add tests
    
    * Renamed to ineligibility
    
    * Fix tests
    
    * fix ineligibilityBinding declaration
    
    ---------
    
    Co-authored-by: Marcin Mogiela <marcinmo@kainos.com>

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -13,6 +13,9 @@ const onApplicationStatusEvent = require('../../ffc-ahwr-event/application-statu
 jest.mock('../../ffc-ahwr-event/register-your-interest-event')
 const onRegisterYourInterestEvent = require('../../ffc-ahwr-event/register-your-interest-event')
 
+jest.mock('../../ffc-ahwr-event/ineligibility-event')
+const onIneligibilityEvent = require('../../ffc-ahwr-event/ineligibility-event')
+
 const processEvent = require('../../ffc-ahwr-event/index')
 const mockContext = require('../mock/mock-context')
 
@@ -100,6 +103,19 @@ describe('index function', () => {
 
       expect(onRegisterYourInterestEvent).toHaveBeenCalledTimes(1)
       expect(onRegisterYourInterestEvent).toHaveBeenCalledWith(mockContext, message)
+      expect(mockMonitoringEvent.saveMonitoring).toHaveBeenCalledTimes(0)
+      expect(mockProtectiveMonitoringEvent.saveMonitoringEvent).toHaveBeenCalledTimes(0)
+    })
+  })
+
+  describe('send-exception-event', () => {
+    test('receives message from service bus', async () => {
+      message.name = 'send-ineligibility-event'
+
+      await processEvent(mockContext, message)
+
+      expect(onIneligibilityEvent).toHaveBeenCalledTimes(1)
+      expect(onIneligibilityEvent).toHaveBeenCalledWith(mockContext, message)
       expect(mockMonitoringEvent.saveMonitoring).toHaveBeenCalledTimes(0)
       expect(mockProtectiveMonitoringEvent.saveMonitoringEvent).toHaveBeenCalledTimes(0)
     })
