0fc990adf937b1420b764c8fd2062dd0f73e7ec1
liamwilson-kainos <liam.wilson@kainos.com>
2023-06-14 10:20:45 +0100

    added coverage

---
+++ test/unit/monitoring/monitoring.test.js
@@ -0,0 +1,39 @@
+describe('Monitoring function', () => {
+  const mockCreateTableClient = require('../../../ffc-ahwr-event/azure-storage/create-table-client')
+  jest.mock('../../../ffc-ahwr-event/azure-storage/create-table-client')
+  const mockLogInfo = jest.fn()
+  test('saveMonitoring', async () => {
+    const context = {
+      bindings: {
+        tableMonitoringBinding: []
+      },
+      log: {
+        info: mockLogInfo
+      }
+    }
+    const event = {
+      properties: {
+        id: '13345',
+        action: {
+          type: 'blah',
+          raisedOn: Date.now(),
+          raisedBy: 'SOmeone'
+
+        },
+        status: 'some status'
+      }
+
+    }
+    const mockCreateTable = jest.fn()
+    mockCreateTableClient.mockImplementationOnce(() => {
+      return {
+        createTable: mockCreateTable
+      }
+    })
+    const monitoring = require('../../../ffc-ahwr-event/monitoring/monitoring')
+    await monitoring.saveMonitoring(context, event)
+    expect(mockCreateTableClient).toHaveBeenCalledTimes(1)
+    expect(mockCreateTable).toHaveBeenCalledTimes(1)
+    expect(mockLogInfo).toHaveBeenCalledTimes(1)
+  })
+})
