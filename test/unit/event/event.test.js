dbb2037ffef517874a44bdfba166dfadf974ae24
Alex White <alex_white742@outlook.com>
2024-05-17 10:31:49 +0100

    Raise invalid data event 371187-378781 (#23)
    * new invalid data event
    
    * 0.8.2
    
    * Fix tests and increase coverage for NOT duplicate event
    
    * Run npm audit fix
    
    * refactor
    
    * updated sonarcloud for tests
    
    ---------
    
    Co-authored-by: JulieLev <JulieLev@users.noreply.github.com>

--- test/unit/event/event.test.js
+++ test/unit/event/event.test.js
@@ -6,6 +6,7 @@ jest.mock('../../../ffc-ahwr-event/azure-storage/query-entities')
 
 describe('Event function', () => {
   const message = {
+    name: 'send-session-event',
     properties: {
       id: '123456789',
       sbi: '123456789',
@@ -33,6 +34,14 @@ describe('Event function', () => {
     await saveEvent(mockContext, message)
     expect(queryEntities).toHaveBeenCalledTimes(1)
     expect(mockContext.bindings).toHaveProperty('tableBinding')
+  })
+
+  test('Duplicated event found NOT created if name is not send-session-event', async () => {
+    queryEntities.mockResolvedValue([{ test: 'test' }])
+    await saveEvent(mockContext, { ...message, name: 'some-other-event' })
+    expect(queryEntities).toHaveBeenCalledTimes(1)
+    expect(mockContext.bindings).toHaveProperty('tableBinding')
+    expect(mockContext.bindings.tableBinding[0].Status).toEqual('in progress')
   })
 
   test('Duplicated event found created', async () => {
