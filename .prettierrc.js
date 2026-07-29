1db5496fec99efff7aa7e5c964f75a9c3ca6b6f9
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2026-07-29 11:13:42 +0100

    Fixing husky setup, adding prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/**
+ * @type {Options}
+ */
+export default {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 100
+}
+
+/**
+ * @import { Options } from 'prettier'
+ */
